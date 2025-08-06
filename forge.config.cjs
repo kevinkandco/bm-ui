edb180c0d879ebc7a2dcd34ed4021c965e890796
infoanil <info.anilchudasama@gmail.com>
2025-08-06 18:36:47 +0530

    ref: file ignored

--- forge.config.cjs
+++ forge.config.cjs
@@ -5,39 +5,56 @@ module.exports = {
   packagerConfig: {
     asar: true,
     arch: "universal",
+      ignore: [
+          /^\/src($|\/)/,
+          /^\/tests?($|\/)/,
+          /^\/\.git($|\/)/,
+          /^\/\.vscode($|\/)/,
+          /^\/node_modules\/\.bin($|\/)/,
+          /^\/node_modules\/.*\/(test|tests|example|docs)($|\/)/,
+          /^\/forge\.config\.cjs$/,
+          /^\/tailwind\.config\.js$/,
+          /^\/postcss\.config\.js$/,
+          /^\/vite\.config\.ts$/,
+          /^\/eslint.*/,
+          /^\/.*\.map$/,
+          /^\/.*\.log$/,
+          /^\/.*\.md$/,
+          /^\/package-lock\.json$/,
+      ],
   },
   rebuildConfig: {},
   makers: [
-    // {
-    //   name: "@electron-forge/maker-deb",
-    //   config: {
-    //     options: {
-    //       name: "Brief Me",
-    //       // maintainer: "Octal infotech",
-    //       // homepage: "https://octalinfotech.com",
-    //       name: "Brief Me",
-    //       title: "Brief Me Installer",
-    //       description: "Smart summaries. Focused updates. Less noise.",
-    //       icon: "public/assets/icon.png",
-    //     },
-    //   },
-    // },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          name: "Brief Me",
+          // maintainer: "Octal infotech",
+          // homepage: "https://octalinfotech.com",
+          name: "Brief Me",
+          title: "Brief Me Installer",
+          description: "Smart summaries. Focused updates. Less noise.",
+          icon: "public/assets/icon.png",
+        },
+      },
+    },
     // {
     //   name: "@electron-forge/maker-squirrel",
     //   config: {},
     // },
-    {
-      name: "@electron-forge/maker-dmg",
-      config: {
-        name: "Brief-me",
-        title: "Brief Me Installer",
-        description: "Smart summaries. Focused updates. Less noise.",
-        icon: "public/assets/mac-logo.icns",
-        // background:
-        //   "public/lovable-uploads/10c02016-8844-4312-a961-c0bfeb309800.png", // optional: background image for the DMG
-        overwrite: true,
-      },
-    },
+    // {
+    //   name: "@electron-forge/maker-dmg",
+    //   config: {
+    //     name: "Brief-me",
+    //     title: "Brief Me Installer",
+    //     description: "Smart summaries. Focused updates. Less noise.",
+    //     icon: "public/assets/mac-logo.icns",
+    //     // background:
+    //     //   "public/lovable-uploads/10c02016-8844-4312-a961-c0bfeb309800.png", // optional: background image for the DMG
+    //     overwrite: true,
+    //   },
+    // },
     // {
     //   name: "@electron-forge/maker-zip",
     //   platforms: ["darwin"],
